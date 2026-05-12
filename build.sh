e0d0c47b85b27fa2031412f565fe5c63109d1796
Saketh <arepalli.saketh76@gmail.com>
2026-05-12 23:39:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
+echo "Force-reinstalling Redis 7.4.0..."
+pip install --upgrade --force-reinstall redis==7.4.0
+
 echo "Downloading FFmpeg..."
 mkdir -p ffmpeg_bin
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C ffmpeg_bin
+
+chmod +x ffmpeg_bin/ffmpeg ffmpeg_bin/ffprobe
