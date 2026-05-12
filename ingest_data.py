95c1f085237c2b848cb33e2e4d46fd75c8976679
Saketh <arepalli.saketh76@gmail.com>
2026-05-12 19:04:32 +0530

    Update ingest_data.py

--- ingest_data.py
+++ ingest_data.py
@@ -1,16 +1,15 @@
+import os
 from services.ingestion import IngestionEngine
 from services.fingerprinter import ConstellationExtractor
 from services.db_manager import RedisManager
 
-# Matches the folder name in image_955257.png
-DATASET_DIRECTORY = "./data"
+DATASET_DIRECTORY = os.path.join(os.path.dirname(__file__), "data")
 
 if __name__ == "__main__":
     db = RedisManager()
     ext = ConstellationExtractor()
     engine = IngestionEngine(ext, db)
 
-    print(f"Cleaning database and starting ingestion from: {DATASET_DIRECTORY}")
-    db.client.flushall()  # Wipe old data for a clean start
+    print(f"Starting ingestion from: {DATASET_DIRECTORY}")
     engine.process_dataset(DATASET_DIRECTORY)
     print("Ingestion complete!")
